4487b5a14e0c208e0bebdffc49d0f7f19e0af064
yadira <yadira.villa@andradescompany.com.mx>
2019-04-08 10:50:52 -0500

    Lista principal de clientes

---
+++ Projects/Factory-Mobile/Soluciones/Factory/Factory/Views/CDetallesGeneral.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Factory.Views
+{
+    public partial class CDetallesGeneral : ContentPage
+    {
+        public CDetallesGeneral()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projects/Factory-Mobile/Soluciones/Factory/Factory/Views/Clientes.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Factory.Modelos;
+using Factory.Services;
+using Xamarin.Forms;
+
+namespace Factory.Views
+{
+    public partial class Clientes : ContentPage
+    {
+        public List<ModelListClientes> CliList = new List<ModelListClientes>();
+        public Clientes()
+        {
+            InitializeComponent();
+            //ListaClientes.ItemsSource = new List<ModelListClientes>
+            //{
+            //    new ModelListClientes { numero = "1", nombre = "Salinas Vargas Katerine ", cupo= "5,000", saldo ="10.00" },
+            //    new ModelListClientes { numero = "2", nombre = "Valverde Lopez Gloria ", cupo= "5,000", saldo ="0.00"},
+            //};
+            _ = InitializeDataAsync();
+        }
+
+        public async Task InitializeDataAsync()
+        {
+            var client1 = new ClienteServices();
+            ListaClientes.ItemsSource = CliList = await client1.GetCliListClienteAsync(Global.numvendedor);
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var cliente = e.SelectedItem as ModelListClientes;
+            DisplayAlert("Selected", "Cliente " + cliente.nombre,"OK");
+
+            if (e.SelectedItem != null)
+            {
+                ModelProducto modelProducto = (ModelProducto)e.SelectedItem;
+                Navigation.PushAsync(new CDetallesGeneral(cliente));
+            }
+                    ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
